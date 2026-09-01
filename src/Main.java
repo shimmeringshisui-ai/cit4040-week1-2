84cc5fa89ef2ed3aca91a850d7daf27c262e89bd
shimmeringshisui <shimmeringshisui@gmail.com>
2026-09-01 23:36:35 +0600

    Session 4: task 4.3 book stock map

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+
 
         Book book1 = new Book("1984", "George Orwell", 328);
         Book book2 = new Book("The Hobbit", "J.R.R. Tolkien", 310);
@@ -29,6 +32,37 @@ public class Main {
             }
         }
 
-        System.out.println("Books with more than 300 pages: " + countOver300);
+        System.out.println(
+                "Books with more than 300 pages: " + countOver300
+        );
+
+
+
+        Map<String, Integer> stock = new HashMap<>();
+
+        stock.put("1984", 5);
+        stock.put("The Hobbit", 3);
+        stock.put("The Little Prince", 7);
+        stock.put("Pride and Prejudice", 4);
+
+        System.out.println(
+                "Copies of The Hobbit: " + stock.get("The Hobbit")
+        );
+
+        System.out.println("Book stock:");
+
+        for (Map.Entry<String, Integer> entry : stock.entrySet()) {
+            System.out.println(
+                    entry.getKey() + ": " + entry.getValue()
+            );
+        }
+
+        int totalCopies = 0;
+
+        for (int copies : stock.values()) {
+            totalCopies += copies;
+        }
+
+        System.out.println("Total number of copies: " + totalCopies);
     }
 }
