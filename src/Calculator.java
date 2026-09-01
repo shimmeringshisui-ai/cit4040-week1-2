cf219c90c32bd69130b0db1d295ec094b70c6b8b
shimmeringshisui <shimmeringshisui@gmail.com>
2026-09-01 22:52:55 +0600

    Session 3: task 3.2 calculate total

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
+
     public static void main(String[] args) {
+
         int quantity = 5;
         double price = 19.99;
         boolean inStock = true;
@@ -13,5 +15,26 @@ public class Calculator {
                         ", in stock: " + inStock +
                         ", category: " + category
         );
+
+        System.out.println("Total 1: " + calculateTotal(20.0, 2));
+        System.out.println("Total 2: " + calculateTotal(20.0, 10));
+        System.out.println("Total 3: " + calculateTotal(15.5, 12));
+        System.out.println("Total 4: " + calculateTotal(-5.0, 3));
+    }
+
+    public static double calculateTotal(double price, int quantity) {
+
+        if (price < 0 || quantity < 0) {
+            System.out.println("Warning: price or quantity cannot be negative.");
+            return 0;
+        }
+
+        double total = price * quantity;
+
+        if (quantity >= 10) {
+            total = total * 0.90;
+        }
+
+        return total;
     }
 }
